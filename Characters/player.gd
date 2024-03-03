fc98d158cc5a8284285674d842645edde91b4f39
AstralDuckster <reesechan80@gmail.com>
2024-03-03 21:58:50 +0800

    Adjusted player movement

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,38 +1,67 @@
 extends CharacterBody2D
 
+const SPEED = 300.0
+const JUMP = -300.0
+const GRAVITY = 700
+const JUMP_VELOCITY = 100
 
-@export var speed : float = 300.0
-@export var jump_velocity : float = -400.0
-@export var double_jump_velocity : float = -100
+enum State { Idle, Run, Jump }
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var has_double_jumped: bool = false
+var current_state
 
-
+func _ready():
+	current_state = State.Idle
+	
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	else:
-		has_double_jumped = false
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			#normal jump 
-			velocity.y = jump_velocity
-		elif not has_double_jumped:
-			#double jump
-			velocity.y = double_jump_velocity
-			has_double_jumped = true
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-
+	player_falling(delta)
+	player_idle(delta)
+	player_run(delta)
+	player_jump(delta)
+	
 	move_and_slide()
+	
+	player_animations()
+	
+	print("State: ",State.keys()[current_state])
+	
+func player_falling(delta):
+	if !is_on_floor():
+		velocity.y += GRAVITY * delta
+
+func player_idle(delta):
+	if is_on_floor():
+		current_state = State.Idle
+
+func player_run(delta):
+	if !is_on_floor():
+		return
+		
+	var direction = Input.get_axis("left", "right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if direction != 0:
+		current_state = State.Run
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+
+func player_jump(delta):
+	if Input.is_action_just_pressed("jump"):
+		current_state = State.Jump
+		if is_on_floor():
+			velocity.y = JUMP
+		
+		
+	if !is_on_floor() and current_state == State.Jump:
+		var direction = Input.get_axis("left", "right")
+		velocity.x += direction * JUMP_VELOCITY * delta
+		
+func player_animations():
+	if current_state == State.Idle:
+		$AnimatedSprite2D.animation = "idle"
+	elif current_state == State.Run:
+		$AnimatedSprite2D.animation = "run"
+	elif current_state == State.Jump:
+		$AnimatedSprite2D.animation = "jump"
